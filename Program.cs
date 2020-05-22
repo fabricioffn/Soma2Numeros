452390711ae78adc1ab90c54c9759f17167fd995
fabricio <fnmamao@gmail.com>
2020-05-22 09:04:38 -0300

    exercicio adicionado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorum = 9;
+            int valordois = 10;
+            int soma;
+            Console.WriteLine("Calculo da soma entre dois numeros");
+
+            soma = valorum + valordois;
+            Console.WriteLine("9 + 10 = " + soma);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Soma2Numeros
+Leia 2 números inteiros e exiba o resultado da soma:
